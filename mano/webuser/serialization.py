3ac916f1b1196068e075e2f4959a3383807f6f3f
egoama <elghoul.oussama@gmail.com>
2025-02-02 12:42:29 +0300

    Update serialization.py

--- mano/webuser/serialization.py
+++ mano/webuser/serialization.py
@@ -6,7 +6,6 @@ By using this software you agree to the terms of the MANO/SMPL+H Model license h
 More information about MANO/SMPL+H is available at http://mano.is.tue.mpg.de.
 For comments or questions, please email us at: mano@tue.mpg.de
 
-
 About this file:
 ================
 This file defines a wrapper for the loading functions of the MANO model.
@@ -15,57 +14,50 @@ Modules included:
 - load_model:
   loads the MANO model from a given file location (i.e. a .pkl file location),
   or a dictionary object.
-
 '''
-
 
 __all__ = ['load_model', 'save_model']
 
 import numpy as np
+import jax.numpy as jnp
 import pickle
-import chumpy as ch
-from chumpy.ch import MatVecMult
 from mano.webuser.posemapper import posemap
 from mano.webuser.verts import verts_core
 
 def ready_arguments(fname_or_dict):
 
     if not isinstance(fname_or_dict, dict):
-        dd = pickle.load(open(fname_or_dict, 'rb'), encoding='latin1')
+        with open(fname_or_dict, 'rb') as f:
+            dd = pickle.load(f, encoding='latin1')
     else:
         dd = fname_or_dict
-
-    backwards_compatibility_replacements(dd)
 
     want_shapemodel = 'shapedirs' in dd
     nposeparms = dd['kintree_table'].shape[1] * 3
 
-    if 'trans' not in dd:
-        dd['trans'] = np.zeros(3)
-    if 'pose' not in dd:
-        dd['pose'] = np.zeros(nposeparms)
+    dd.setdefault('trans', np.zeros(3))
+    dd.setdefault('pose', np.zeros(nposeparms))
+    
     if 'shapedirs' in dd and 'betas' not in dd:
         dd['betas'] = np.zeros(dd['shapedirs'].shape[-1])
 
-    for s in [
-            'v_template', 'weights', 'posedirs', 'pose', 'trans', 'shapedirs',
-            'betas', 'J'
-    ]:
-        if (s in dd) and not hasattr(dd[s], 'dterms'):
-            dd[s] = ch.array(dd[s])
+    for s in ['v_template', 'weights', 'posedirs', 'pose', 'trans', 'shapedirs', 'betas', 'J']:
+        if s in dd:
+            dd[s] = jnp.array(dd[s])  # Replacing chumpy.array with jax.numpy.array
 
     if want_shapemodel:
-        dd['v_shaped'] = dd['shapedirs'].dot(dd['betas']) + dd['v_template']
+        dd['v_shaped'] = dd['shapedirs'] @ dd['betas'] + dd['v_template']
         v_shaped = dd['v_shaped']
-        J_tmpx = MatVecMult(dd['J_regressor'], v_shaped[:, 0])
-        J_tmpy = MatVecMult(dd['J_regressor'], v_shaped[:, 1])
-        J_tmpz = MatVecMult(dd['J_regressor'], v_shaped[:, 2])
-        dd['J'] = ch.vstack((J_tmpx, J_tmpy, J_tmpz)).T
-        dd['v_posed'] = v_shaped + dd['posedirs'].dot(
-            posemap(dd['bs_type'])(dd['pose']))
+        
+        # Replacing MatVecMult with standard matrix multiplication
+        J_tmpx = dd['J_regressor'] @ v_shaped[:, 0]
+        J_tmpy = dd['J_regressor'] @ v_shaped[:, 1]
+        J_tmpz = dd['J_regressor'] @ v_shaped[:, 2]
+        
+        dd['J'] = jnp.vstack((J_tmpx, J_tmpy, J_tmpz)).T
+        dd['v_posed'] = v_shaped + dd['posedirs'] @ posemap(dd['bs_type'])(dd['pose'])
     else:
-        dd['v_posed'] = dd['v_template'] + dd['posedirs'].dot(
-            posemap(dd['bs_type'])(dd['pose']))
+        dd['v_posed'] = dd['v_template'] + dd['posedirs'] @ posemap(dd['bs_type'])(dd['pose'])
 
     return dd
 
@@ -79,7 +71,7 @@ def load_model(fname_or_dict):
         'J': dd['J'],
         'weights': dd['weights'],
         'kintree_table': dd['kintree_table'],
-        'xp': ch,
+        'xp': jnp,  # Replacing chumpy with jax.numpy
         'want_Jtr': True,
         'bs_style': dd['bs_style']
     }
